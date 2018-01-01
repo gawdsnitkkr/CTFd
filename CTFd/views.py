5f7d95a725caec4394f8e1bd568ecd68aab4ad42
Divya Mamgai <divyamamgai21@gmail.com>
2018-01-01 12:44:49 +0530

    Updated Theme

--- CTFd/views.py
+++ CTFd/views.py
@@ -40,18 +40,39 @@ def setup():
 
             index = """<div class="row">
     <div class="col-md-6 offset-md-3">
-        <img class="w-100 mx-auto d-block" style="max-width: 500px;padding: 50px;padding-top: 14vh;" src="themes/core/static/img/logo.png" />
-        <h3 class="text-center">
-            <p>A cool CTF platform from <a href="https://ctfd.io">ctfd.io</a></p>
-            <p>Follow us on social media:</p>
-            <a href="https://twitter.com/ctfdio"><i class="fab fa-twitter fa-2x" aria-hidden="true"></i></a>&nbsp;
-            <a href="https://facebook.com/ctfdio"><i class="fab fa-facebook fa-2x" aria-hidden="true"></i></a>&nbsp;
-            <a href="https://github.com/ctfd"><i class="fab fa-github fa-2x" aria-hidden="true"></i></a>
+        <h3 class="text-center" style="margin-top: 30px">
+            BEAT THE GAWDS
         </h3>
-        <br>
-        <h4 class="text-center">
-            <a href="admin">Click here</a> to login and setup your CTF
+        <h4 class="text-center" style="margin-bottom: 30px">
+            Capture The Flag
         </h4>
+        <h6 class="text-center" style="margin-bottom: 15px">
+            Some help full links to get you started!
+        </h6>
+        <div class="text-center">
+            <a href="https://ctftime.org/ctf-wtf/" target="_blank"
+               title="CTFtime.org / All about CTF (Capture The Flag)">
+                CTFtime.org / All about CTF (Capture The Flag)
+            </a>
+        </div>
+        <div class="text-center">
+            <a href="https://www.endgame.com/blog/technical-blog/how-get-started-ctf" target="_blank"
+               title="How to Get Started in CTF | Endgame">
+                How to Get Started in CTF | Endgame
+            </a>
+        </div>
+        <div class="text-center">
+            <a href="https://trailofbits.github.io/ctf/" target="_blank"
+               title="Introduction · CTF Field Guide">
+                Introduction · CTF Field Guide
+            </a>
+        </div>
+        <div class="text-center">
+            <a href="http://www.openctf.com/html/firstctf.html" target="_blank"
+               title="How to play your first OpenCTF">
+               How to play your first OpenCTF
+            </a>
+        </div>
     </div>
 </div>""".format(request.script_root)
 
